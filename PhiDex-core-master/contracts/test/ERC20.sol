7b4ad84673f2596e440ed634ee2a38f4a3655782
phidex <83309249+phidex@users.noreply.github.com>
2021-06-09 17:30:37 +0300

    Delete PhiDex-core-master directory

--- PhiDex-core-master/contracts/test/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../PhidexV2ERC20.sol';
-
-contract ERC20 is PhidexV2ERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
